e311457b816ac7ce76783393bf79ca4bd43d1416
denizdogruel00 <denizyazilm@gmail.com>
2026-02-14 09:34:46 +0300

    first commit

---
+++ src/components/Urun.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+function Urun(){
+    return(
+       <table className="table table-bordered">
+        <tr>
+            <th>id</th>
+            <th>Adi</th>
+        </tr>
+       </table>
+    )
+}export default Urun
