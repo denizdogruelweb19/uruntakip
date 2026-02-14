31e7be955cd1e244c0e887f49a7a70dfae85eebc
denizdogruel00 <denizyazilm@gmail.com>
2026-02-14 09:56:01 +0300

    veriler tabloda listelendi

--- src/components/Urun.tsx
+++ src/components/Urun.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 function Urun({products}){
+
     return(
        <table className="table table-bordered">
         <tr>
             <th>id</th>
             <th>Adi</th>
+            <th>Detay</th>
         </tr>
         {products.map((product,index)=>{
-            <tr key={index}>
-                <td>{index}</td>
-                <td>{product}</td>
+            return<tr >
+              <td>{index}</td>
+                <td key={index}>{product}</td>
+                <td>Detay</td>
             </tr>
         })}
        </table>
