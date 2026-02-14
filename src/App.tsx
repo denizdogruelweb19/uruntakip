82a36df4dea21ad50d6b55e8d2db6bb1c49e0f10
denizdogruel00 <denizyazilm@gmail.com>
2026-02-14 09:36:12 +0300

    props olustu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import Urun from "./components/Urun"
 
 
 function App() {
  
-
+const products=["iphone 11","Apple watch","samsung s8"]
   return (
     <>
       <div>
         
-        
+        <Urun></Urun>
         
         </div>
         
