6690274957d866db102c9eb3da3e93d585b0012f
denizdogruel00 <denizyazilm@gmail.com>
2026-02-14 09:57:54 +0300

    ürünlerin propsu olustu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Urun from "./components/Urun"
-
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 function App() {
  
-const products=["iphone 11","Apple watch","samsung s8"]
+const products=["iphone 11","Apple watch","samsung s8"];
   return (
     <>
       <div>
